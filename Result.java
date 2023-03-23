431ea89cd09cc690fd1677dc04eb8969def3f571
LaxmiGundeti <127479077+LaxmiGundeti@users.noreply.github.com>
2023-03-23 22:14:20 +0530

    Add files via upload
    practicals of 23 March 2023

---
+++ Result.java
@@ -0,0 +1,26 @@
+class Calculator
+{
+    int a,b;
+	Calculator(int a,int b) //parameterized constructor
+	{
+	   this.a=a;
+	   this.b=b;
+	}
+	void display()  
+    {
+       System.out.println("Addition="+(a+b)); //addition
+	   System.out.println("Substraction="+(a-b)); //substraction
+	   System.out.println("Multiplication="+(a*b)); //multiplication
+	   System.out.println("Division="+(a/b));  //Division
+	  
+    }
+}
+class Result  //main class
+{
+	public static void main(String args[])  
+	{
+		Calculator c1=new Calculator(30,50); //created an object
+		c1.display();  //call display method
+	}
+
+}
